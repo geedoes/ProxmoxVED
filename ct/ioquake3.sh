46c7881145e2cf4a63abac6fb8fec34383751431
geedoes <42254476+geedoes@users.noreply.github.com>
2026-04-06 14:15:58 -0500

    Update ioquake3.sh

--- ct/ioquake3.sh
+++ ct/ioquake3.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVED/main/misc/build.func)
 
 # Copyright (c) 2021-2026 community-scripts ORG
 # Author: geedoes
@@ -21,15 +20,15 @@ color
 catch_errors
 
 function update_script() {
-    header_info
-    check_container_storage
-    check_container_resources
-    if [[ ! -d /opt/ioquake3 ]]; then
-        msg_error "No ${APP} Installation Found!"
-        exit
-    fi
-    msg_ok "No update required. ${APP} does not currently support auto-updates."
+  header_info
+  check_container_storage
+  check_container_resources
+  if [[ ! -d /opt/ioquake3 ]]; then
+    msg_error "No ${APP} Installation Found!"
     exit
+  fi
+  msg_ok "No update required. ${APP} does not currently support auto-updates."
+  exit
 }
 
 start
@@ -41,6 +40,6 @@ echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
 echo -e "${INFO}${YW} IMPORTANT: Manual File Deployment Required${CL}"
 echo -e "${TAB}${BGN}1.${CL} Upload your 'pak0.pk3' to Proxmox ISO storage, renamed as ${BL}pak0.iso${CL}"
 echo -e "${TAB}${BGN}2.${CL} Run the following command on your Proxmox Host to deploy it:"
-echo -e "${TAB}${TAB}${GN}pct push $CTID /var/lib/vz/template/iso/pak0.iso /opt/ioquake3/baseq3/pak0.pk3${CL}"
+echo -e "${TAB}${TAB}${GN}pct push ${CTID} /var/lib/vz/template/iso/pak0.iso /opt/ioquake3/baseq3/pak0.pk3${CL}"
 echo -e "${TAB}${BGN}3.${CL} Restart the service:"
-echo -e "${TAB}${TAB}${GN}pct exec $CTID -- systemctl restart ioquake3${CL}"
+echo -e "${TAB}${TAB}${GN}pct exec ${CTID} -- systemctl restart ioquake3${CL}"
