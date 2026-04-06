5e93db792f0183b4e472dccfb8a42cf382afae46
geedoes <42254476+geedoes@users.noreply.github.com>
2026-04-06 14:16:12 -0500

    Update ioquake3.sh

--- ct/ioquake3.sh
+++ ct/ioquake3.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVED/main/misc/build.func)
 
 # Copyright (c) 2021-2026 community-scripts ORG
 # Author: geedoes
