5960c91549a5b78a7119b7f96f5c9e8352f0504d
geedoes <42254476+geedoes@users.noreply.github.com>
2026-04-06 14:09:55 -0500

    Update ioquake3.sh

--- ct/ioquake3.sh
+++ ct/ioquake3.sh
@@ -4,6 +4,7 @@ source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxV
 # Copyright (c) 2021-2026 community-scripts ORG
 # Author: geedoes
 # License: MIT | https://github.com/community-scripts/ProxmoxVED/raw/main/LICENSE
+# Source: https://github.com/ioquake/ioq3
 
 APP="ioquake3"
 var_tags="${var_tags:-game;server}"
